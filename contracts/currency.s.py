961cdb8c41efb0e13376c50b2b97756e4dae3e67
Endogen <Endogen@users.noreply.github.com>
2026-03-31 19:11:17 +0200

    Extract permit authorizer from currency

--- contracts/currency.s.py
+++ contracts/currency.s.py
@@ -1,6 +1,5 @@
 balances = Hash(default_value=0)
 metadata = Hash()
-permits = Hash()
 
 TransferEvent = LogEvent(
     "Transfer",
@@ -33,6 +32,7 @@ def seed(vk: str):
     metadata["token_logo_url"] = "https://xian.org/assets/img/logo.svg"
     metadata["token_website"] = "https://xian.org"
     metadata["operator"] = "team_lock"
+    metadata["permit_authorizer"] = "permit_authorizer"
 
 
 @export
@@ -61,6 +61,20 @@ def approve(amount: float, to: str):
 
 
 @export
+def approve_from_authorizer(owner: str, spender: str, amount: float):
+    authorizer = metadata["permit_authorizer"] or "permit_authorizer"
+
+    assert (
+        ctx.caller == authorizer
+    ), "Only permit authorizer can approve on behalf of others."
+    assert amount >= 0, "Cannot approve negative balances."
+
+    balances[owner, spender] = amount
+
+    ApproveEvent({"from": owner, "to": spender, "amount": amount})
+
+
+@export
 def transfer_from(amount: float, to: str, main_account: str):
     assert amount > 0, "Cannot send negative balances."
     assert (
@@ -78,30 +92,3 @@ def transfer_from(amount: float, to: str, main_account: str):
 @export
 def balance_of(address: str):
     return balances[address]
-
-
-@export
-def permit(owner: str, spender: str, value: float, deadline: str, signature: str):
-    deadline = strptime_ymdhms(deadline)
-    permit_msg = construct_permit_msg(owner, spender, value, str(deadline))
-    permit_hash = hashlib.sha3(permit_msg)
-
-    assert permits[permit_hash] is None, "Permit can only be used once."
-    assert now < deadline, "Permit has expired."
-    assert value >= 0, "Cannot approve negative balances!"
-    assert crypto.verify(owner, permit_msg, signature), "Invalid signature."
-
-    balances[owner, spender] = value
-    permits[permit_hash] = True
-
-    ApproveEvent({"from": owner, "to": spender, "amount": value})
-
-    return permit_hash
-
-
-def construct_permit_msg(owner: str, spender: str, value: float, deadline: str):
-    return f"{owner}:{spender}:{value}:{deadline}:{ctx.this}:{chain_id}"
-
-
-def strptime_ymdhms(date_string: str) -> datetime.datetime:
-    return datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
